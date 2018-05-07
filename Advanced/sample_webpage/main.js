7c1fb6dfd19216554661ea37cac2c00a32f6767c
Mario <marioschantel@gmail.com>
2018-05-07 13:21:01 +0200

    Advanced Part

---
+++ Advanced/sample_webpage/main.js
@@ -0,0 +1,21 @@
+$("a").attr("href", "http://www.codefactory.wien");
+
+$("code").css("color", "red");
+
+$("ol > li").css("background-color", "#2a7b90");
+
+$("textarea").val("Express your Opinion");
+
+$("img").attr("src", "https://upload.wikimedia.org/wikipedia/de/thumb/5/50/DFB-Logo_1945.svg/100px-DFB-Logo_1945.svg.png");
+
+$("blockquote").css({"background-color":"orange", "font-style":"italic"});
+
+$("#logo").css("color", "black");
+
+$("h1 > span").removeClass("gray").css("color", "white");
+
+$(".date").remove();
+
+$("#sidebar").append("<ul><li>New Templates</li> <li>Order Templates</li> <li>Contact Us</li> </ul>");
+
+$("p:contains('Be not afraid of life. Believe that life is worth living, and your belief will help create the fact.')").text("When someone says: I want a programming language in which I need only to say what I wish, and done, give him a lollipop.");
