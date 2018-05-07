c48b6bcf55838a80477d8957f174b24db290706f
Mario <marioschantel@gmail.com>
2018-05-07 11:04:01 +0200

    intermediate exercises

---
+++ Intermediate/manipulating_html_page_exercise_files/myscript.js
@@ -0,0 +1,18 @@
+$("form").submit(function(e) {
+
+	e.preventDefault();
+	
+	
+	
+
+});
+
+/*$("form").remove();*/
+
+$("article").append("<div>Div1</div>", "<div>Div1</div>", "<div>Div1</div>", "<div>Div1</div>", "<div>Div1</div>");
+
+$("input").val("Search for ...");
+
+$("div").addClass("box");
+
+$("a").attr("href", "http://www.codefactory.wien");
